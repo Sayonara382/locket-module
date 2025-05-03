42f1c2bf56d19549675bc11c1bf38c58d7ea08d8
Thuan Truong <qthuan382@gmail.com>
2025-05-03 09:40:05 +0700

    Update overwrite.js

--- overwrite.js
+++ overwrite.js
@@ -1,3 +1,9 @@
-let headers = $request.headers;
-delete headers['Authorization'];
-$done({ headers });
+function setHeaderValue(headers, key, value) {
+  const lowerKey = key.toLowerCase();
+  headers.hasOwnProperty(lowerKey) ? headers[lowerKey] = value : headers[key] = value;
+}
+
+const modifiedHeaders = { ...$request.headers };
+setHeaderValue(modifiedHeaders, 'X-RevenueCat-ETag', '');
+
+$done({ headers: modifiedHeaders });
