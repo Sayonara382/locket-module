521e93480741a5ae78fc011adfe6ad645c38bfa8
Thuan Truong <qthuan382@gmail.com>
2025-08-21 16:50:26 +0700

    Update purchase.js

--- purchase.js
+++ purchase.js
@@ -1,45 +1,28 @@
-(function () {
-  const mapping = {
-    '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-    'Locket': ['Gold']
+if (body && body.subscriber) {
+  body.subscriber.subscriptions = body.subscriber.subscriptions || {};
+  body.subscriber.entitlements = body.subscriber.entitlements || {};
+
+  const date = new Date();
+  date.setDate(date.getDate() - 1);
+  const purchaseDate = date.toISOString();
+
+  body.subscriber.entitlements["Gold"] = {
+    expires_date: "2099-12-31T23:59:59Z",
+    product_identifier: "locket_gold_lifetime",
+    purchase_date: purchaseDate
   };
 
-  const userAgent = $request.headers['User-Agent'] || $request.headers['user-agent'];
-  const response = JSON.parse($response.body);
-
-  response.Attention = 'Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!';
-
-  const subscriptionInfo = {
-    is_sandbox: false,
-    ownership_type: 'PURCHASED',
+  body.subscriber.subscriptions["locket_gold_lifetime"] = {
     billing_issues_detected_at: null,
-    period_type: 'normal',
-    expires_date: '2026-05-01T01:04:17Z',
-    grace_period_expires_date: null,
-    unsubscribe_detected_at: null,
-    original_purchase_date: '2025-05-01T01:04:18Z',
-    purchase_date: '2025-05-01T01:04:17Z',
-    store: 'app_store'
+    expires_date: "2099-12-31T23:59:59Z",
+    is_sandbox: false,
+    original_purchase_date: purchaseDate,
+    ownership_type: "PURCHASED",
+    period_type: "lifetime",
+    purchase_date: purchaseDate,
+    store: "app_store",
+    unsubscribe_detected_at: null
   };
+}
 
-  const entitlementInfo = {
-    grace_period_expires_date: null,
-    purchase_date: '2025-05-01T01:04:17Z',
-    product_identifier: 'com.ohoang7.premium.yearly',
-    expires_date: '2026-05-01T01:04:17Z'
-  };
-
-  const matchedKey = Object.keys(mapping).find(key => userAgent.includes(key));
-
-  if (matchedKey) {
-    const [entitlementName] = mapping[matchedKey];
-    entitlementInfo.product_identifier = entitlementName;
-    response.subscriber.subscriptions[entitlementName] = subscriptionInfo;
-    response.subscriber.entitlements[matchedKey] = entitlementInfo;
-  } else {
-    response.subscriber.subscriptions['com.ohoang7.premium.yearly'] = subscriptionInfo;
-    response.subscriber.entitlements['pro'] = entitlementInfo;
-  }
-
-  $done({ body: JSON.stringify(response) });
-})();
+$done({ body: JSON.stringify(body) });
